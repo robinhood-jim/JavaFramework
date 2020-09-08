abb7300ff7d0ed4a1c29229f8a19bdcbe335b027
robinhoodjim <robinjim@126.com>
2020-09-08 21:32:36 +0800

    ElasticSearch Comm Query Tool

---
+++ estool/src/main/java/com/robin/es/util/SchemaCache.java
@@ -0,0 +1,64 @@
+package com.robin.es.util;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.RemovalListener;
+import com.google.common.cache.RemovalNotification;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class SchemaCache {
+    private static Cache<String, Map<String,Object>> cache= CacheBuilder.newBuilder().initialCapacity(100).expireAfterWrite(30, TimeUnit.HOURS)
+            .removalListener(new RemovalListener<String, Map>() {
+                @Override
+                public void onRemoval(RemovalNotification<String, Map> removalNotification) {
+                    //esClusterMap.remove(removalNotification.getKey());
+                }
+            })
+            .build();
+    private static final Map<String,Map<String,Object>> esClusterMap=new HashMap<>();
+
+    public static void registerEsCluster(String clusterName,String clusterUrl,String userName,String passwd){
+        Map<String,Object> cfgMap=new HashMap<>();
+        cfgMap.put("url",clusterUrl);
+        int startpos=clusterUrl.startsWith("https:")?8:7;
+        int endpos=clusterUrl.endsWith("/")?clusterUrl.length()-1:clusterUrl.length();
+        String[] str=clusterUrl.substring(startpos,endpos).split(":");
+        cfgMap.put("host",str[0]);
+        cfgMap.put("port",str[1]);
+        if(null!=userName &&!StringUtils.isEmpty(userName)){
+            cfgMap.put("userName",userName);
+        }
+        if(null!=passwd &&!StringUtils.isEmpty(passwd)){
+            cfgMap.put("userName",passwd);
+        }
+        esClusterMap.put(clusterName,cfgMap);
+        Map<String,Object> map=SchemaAwareUtil.getIndexs(clusterUrl);
+        cache.put(clusterName,map);
+    }
+    public static Map<String,Object> getIndexDefine(String clusterName,String indexName){
+        if(esClusterMap.containsKey(clusterName)){
+            Map<String,Object> schemaMap=cache.getIfPresent(clusterName);
+            if(null==schemaMap){
+                String url=esClusterMap.get(clusterName).get("url").toString();
+                if(!url.endsWith("/")){
+                    url+="/_all";
+                }
+                schemaMap=SchemaAwareUtil.getIndexs(url);
+                cache.put(clusterName,schemaMap);
+            }
+            if(schemaMap.containsKey(indexName.toLowerCase())){
+                return (Map<String,Object>)schemaMap.get(indexName.toLowerCase());
+            }
+            return null;
+        }
+        return null;
+    }
+
+    public static Map<String, Map<String, Object>> getEsClusterMap() {
+        return esClusterMap;
+    }
+}
