5a8c3dd0bf6a07485c33587ff8fc21bc522f72f9
robinhoodjim <robinjim@126.com>
2021-07-23 16:22:22 +0800

    missing class

---
+++ tracer/src/main/java/com/robin/tracer/utils/TracerConstant.java
@@ -0,0 +1,17 @@
+package com.robin.tracer.utils;
+
+/**
+ * <p>Project:  frame</p>
+ *
+ * <p>Description: TracerConstant </p>
+ *
+ * <p>Copyright: Copyright (c) 2021 modified at 2021-07-01</p>
+ *
+ *
+ * @author luoming
+ * @version 1.0
+ */
+public class TracerConstant {
+    public static final String APPNAME_KEY="spring.application.name";
+    public static final String ENABLE_DATASOURCETRACE="tracer.datasource";
+}
