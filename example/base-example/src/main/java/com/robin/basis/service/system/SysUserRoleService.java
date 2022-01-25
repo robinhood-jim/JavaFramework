1cab080fed30c9968002ea02bca80af336656403
robinhoodjim <robinjim@126.com>
2022-01-25 15:13:00 +0800

    jdbcDao and exmaple basis modify

---
+++ example/base-example/src/main/java/com/robin/basis/service/system/SysUserRoleService.java
@@ -0,0 +1,9 @@
+package com.robin.basis.service.system;
+
+import com.robin.core.base.service.BaseAnnotationJdbcService;
+import com.robin.basis.model.user.SysUserRole;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SysUserRoleService extends BaseAnnotationJdbcService<SysUserRole,Long> {
+}
