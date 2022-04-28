90f058d86805552b21586ae3a11f240f89027f36
robinhoodjim <robinjim@126.com>
2022-04-28 14:38:26 +0800

    complete ProtoBuf Writer

--- hadooptool/src/main/java/com/robin/comm/fileaccess/writer/ProtoBufFileWriter.java
+++ hadooptool/src/main/java/com/robin/comm/fileaccess/writer/ProtoBufFileWriter.java
@@ -3,10 +3,8 @@ package com.robin.comm.fileaccess.writer;
 
 import com.github.os72.protobuf.dynamic.DynamicSchema;
 import com.github.os72.protobuf.dynamic.MessageDefinition;
-
 import com.google.protobuf.Descriptors;
 import com.google.protobuf.DynamicMessage;
-import com.google.protobuf.ExtensionRegistry;
 import com.robin.comm.fileaccess.util.ProtoBufUtil;
 import com.robin.core.fileaccess.meta.DataCollectionMeta;
 import com.robin.core.fileaccess.meta.DataSetColumnMeta;
@@ -22,11 +20,11 @@ import java.util.Map;
 public class ProtoBufFileWriter extends AbstractFileWriter {
     private DynamicSchema schema;
     private DynamicMessage message;
-    private ExtensionRegistry registry;
+    //private ExtensionRegistry registry;
     private DynamicSchema.Builder schemaBuilder;
     private DynamicMessage.Builder mesgBuilder;
     private MessageDefinition definition;
-    protected ProtoBufFileWriter(DataCollectionMeta colmeta) {
+    public ProtoBufFileWriter(DataCollectionMeta colmeta) {
         super(colmeta);
     }
 
@@ -46,7 +44,6 @@ public class ProtoBufFileWriter extends AbstractFileWriter {
                 schemaBuilder.addMessageDefinition(definition);
                 schema = schemaBuilder.build();
                 mesgBuilder= DynamicMessage.newBuilder(schema.getMessageDescriptor(colmeta.getValueClassName()));
-                registry=getExtension(schema,colmeta);
             }else{
                 checkAccessUtil(null);
                 out = accessUtil.getOutResourceByStream(colmeta, ResourceUtil.getProcessPath(colmeta.getPath()));
@@ -55,13 +52,7 @@ public class ProtoBufFileWriter extends AbstractFileWriter {
             ex.printStackTrace();
         }
     }
-    public static  ExtensionRegistry getExtension(DynamicSchema schema, DataCollectionMeta colmeta){
-        ExtensionRegistry extensionRegistry = ExtensionRegistry.newInstance();
-        for(Descriptors.FieldDescriptor descriptor:schema.getMessageDescriptor(colmeta.getValueClassName()).getFields()){
-            extensionRegistry.add(descriptor);
-        }
-        return extensionRegistry;
-    }
+
 
     @Override
     public void finishWrite() throws IOException {
@@ -75,8 +66,8 @@ public class ProtoBufFileWriter extends AbstractFileWriter {
 
     @Override
     public void writeRecord(Map<String, Object> map) throws IOException, OperationNotSupportedException {
-        DynamicMessage.Builder msgBuilder=schema.newMessageBuilder(colmeta.getClassNamespace());
-        Descriptors.Descriptor msgDesc=msgBuilder.getDescriptorForType();
+        DynamicMessage.Builder msgBuilder=schema.newMessageBuilder(colmeta.getValueClassName());
+        Descriptors.Descriptor msgDesc=schema.getMessageDescriptor(colmeta.getValueClassName());
         Iterator<Map.Entry<String,Object>> iter=map.entrySet().iterator();
         while(iter.hasNext()){
             Map.Entry<String,Object> entry=iter.next();
