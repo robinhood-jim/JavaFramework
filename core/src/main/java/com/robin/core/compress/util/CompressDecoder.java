b37ab7ebd5979924173b73d0804a4b01ab877af8
robinhoodjim <robinhoodjim7704@gmail.com>
2024-12-20 21:06:06 +0800

    optimize code

--- core/src/main/java/com/robin/core/compress/util/CompressDecoder.java
+++ core/src/main/java/com/robin/core/compress/util/CompressDecoder.java
@@ -10,75 +10,63 @@ import org.anarres.lzo.LzoLibrary;
 import org.apache.commons.compress.compressors.brotli.BrotliCompressorInputStream;
 import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
 import org.apache.commons.compress.compressors.zstandard.ZstdCompressorInputStream;
-import org.tukaani.xz.LZMAInputStream;
 import org.tukaani.xz.XZInputStream;
 import org.xerial.snappy.SnappyInputStream;
 
-import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.ZipInputStream;
 
 
 public class CompressDecoder {
-    private CompressDecoder(){
+    private CompressDecoder() {
 
     }
-    public static InputStream getInputStreamByCompressType(String path, InputStream rawstream) throws IOException{
+
+    public static InputStream getInputStreamByCompressType(String path, InputStream rawstream) throws IOException {
         InputStream inputStream;
-        FileUtils.FileContent content=FileUtils.parseFile(path);
-        Const.CompressType type=content.getCompressType();
-        switch (type){
+        FileUtils.FileContent content = FileUtils.parseFile(path);
+        Const.CompressType type = content.getCompressType();
+        switch (type) {
             case COMPRESS_TYPE_GZ:
-                inputStream=new GZIPInputStream(wrapInputStream(rawstream));
+                inputStream = new GZIPInputStream(rawstream);
                 break;
             case COMPRESS_TYPE_BZ2:
-                inputStream=new BZip2CompressorInputStream(wrapInputStream(rawstream));
+                inputStream = new BZip2CompressorInputStream(rawstream);
                 break;
             case COMPRESS_TYPE_LZO:
                 LzoAlgorithm algorithm = LzoAlgorithm.LZO1X;
                 LzoDecompressor decompressor = LzoLibrary.getInstance().newDecompressor(algorithm, null);
-                inputStream = new LzoInputStream(wrapInputStream(rawstream), decompressor);
+                inputStream = new LzoInputStream(rawstream, decompressor);
                 break;
             case COMPRESS_TYPE_SNAPPY:
-                inputStream=new SnappyInputStream(wrapInputStream(rawstream));
+                inputStream = new SnappyInputStream(rawstream);
                 break;
             case COMPRESS_TYPE_ZIP:
-                ZipInputStream stream1=new ZipInputStream(wrapInputStream(rawstream));
+                ZipInputStream stream1 = new ZipInputStream(rawstream);
                 stream1.getNextEntry();
-                inputStream=stream1;
+                inputStream = stream1;
                 break;
             case COMPRESS_TYPE_LZ4:
-                inputStream=new LZ4FrameInputStream(wrapInputStream(rawstream));
+                inputStream = new LZ4FrameInputStream(rawstream);
                 break;
             case COMPRESS_TYPE_LZMA:
-                inputStream=new XZInputStream(wrapInputStream(rawstream));
+                inputStream = new XZInputStream(rawstream);
                 break;
             case COMPRESS_TYPE_ZSTD:
-                inputStream=new ZstdCompressorInputStream(wrapInputStream(rawstream));
+                inputStream = new ZstdCompressorInputStream(rawstream);
                 break;
             case COMPRESS_TYPE_BROTLI:
-                inputStream=new BrotliCompressorInputStream(wrapInputStream(rawstream));
+                inputStream = new BrotliCompressorInputStream(rawstream);
                 break;
             case COMPRESS_TYPE_XZ:
-                inputStream=new XZInputStream(wrapInputStream(rawstream));
+                inputStream = new XZInputStream(rawstream);
                 break;
             default:
-                inputStream=wrapInputStream(rawstream);
+                inputStream = rawstream;
 
         }
         return inputStream;
-    }
-    private static InputStream wrapInputStream(InputStream instream){
-        InputStream in=null;
-        if(instream instanceof BufferedInputStream){
-            in=instream;
-        }else{
-            in=new BufferedInputStream(instream);
-        }
-        return in;
     }
 }
--- hadooptool/src/main/java/com/robin/comm/fileaccess/iterator/AvroFileIterator.java
+++ hadooptool/src/main/java/com/robin/comm/fileaccess/iterator/AvroFileIterator.java
@@ -36,184 +36,186 @@ import java.util.List;
 import java.util.Map;
 
 public class AvroFileIterator extends AbstractFileIterator {
-	private Schema schema;
-	private FileReader<GenericRecord> fileReader;
-	private Double allowOffHeapDumpLimit= ResourceConst.ALLOWOUFHEAPMEMLIMIT;
-	private MemorySegment segment;
+    private Schema schema;
+    private FileReader<GenericRecord> fileReader;
+    private Double allowOffHeapDumpLimit = ResourceConst.ALLOWOUFHEAPMEMLIMIT;
+    private MemorySegment segment;
 
-	public AvroFileIterator(){
-		identifier= Const.FILEFORMATSTR.AVRO.getValue();
-	}
-	public AvroFileIterator(DataCollectionMeta colmeta) {
-		super(colmeta);
-		identifier= Const.FILEFORMATSTR.AVRO.getValue();
-	}
-	private File tmpFile;
-	private SeekableInput input=null;
+    public AvroFileIterator() {
+        identifier = Const.FILEFORMATSTR.AVRO.getValue();
+    }
 
-	@Override
-	public boolean hasNext() {
-		try{
-			return fileReader.hasNext();
-		}catch(Exception ex){
-			logger.error("",ex);
-		}
-		return false;
-	}
+    public AvroFileIterator(DataCollectionMeta colmeta) {
+        super(colmeta);
+        identifier = Const.FILEFORMATSTR.AVRO.getValue();
+    }
 
-	@Override
-	public void beforeProcess() {
-		try {
-			schema= AvroUtils.getSchemaFromMeta(colmeta);
-			doInit(colmeta.getPath());
-		}catch (Exception ex){
-			logger.error("Exception {0}",ex);
-		}
-	}
+    private File tmpFile;
+    private SeekableInput input = null;
+
+    @Override
+    public boolean hasNext() {
+        try {
+            return fileReader.hasNext();
+        } catch (Exception ex) {
+            logger.error("", ex);
+        }
+        return false;
+    }
+
+    @Override
+    public void beforeProcess() {
+        try {
+            schema = AvroUtils.getSchemaFromMeta(colmeta);
+            doInit(colmeta.getPath());
+        } catch (Exception ex) {
+            logger.error("Exception {0}", ex);
+        }
+    }
 
 
+    private void doInit(String resourcePath) throws Exception {
+        if (colmeta.getSourceType().equals(ResourceConst.IngestType.TYPE_HDFS.getValue())) {
+            HDFSUtil util = new HDFSUtil(colmeta);
+            instream = util.getHDFSDataByRawInputStream(ResourceUtil.getProcessPath(resourcePath));
+            input = new AvroFSInput(new FSDataInputStream(instream), util.getHDFSFileSize(ResourceUtil.getProcessPath(resourcePath)));
+        } else {
+            // no hdfs input source
+            if (!ResourceConst.IngestType.TYPE_LOCAL.getValue().equals(colmeta.getSourceType())) {
+                instream = accessUtil.getRawInputStream(colmeta, ResourceUtil.getProcessPath(resourcePath));
+                int size = instream.available();
+                Double freeMemory = SysUtils.getFreeMemory();
+                //file size too large ,can not store in ByteBuffer or freeMemory too low
+                if (size >= Integer.MAX_VALUE || freeMemory < allowOffHeapDumpLimit) {
+                    String tmpPath = FileUtils.getTempDirectoryPath() + ResourceUtil.getProcessFileName(resourcePath);
+                    tmpFile = new File(tmpPath);
+                    copyToLocal(tmpFile, instream);
+                    input = new SeekableFileInput(tmpFile);
+                } else {
+                    segment = MemorySegmentFactory.allocateOffHeapUnsafeMemory(size, this, new Thread() {});
+                    ByteBuffer byteBuffer = segment.getOffHeapBuffer();
+                    try (ReadableByteChannel channel = Channels.newChannel(instream)) {
+                        IOUtils.readFully(channel, byteBuffer);
+                        byteBuffer.position(0);
+                    }
+                    input = new SeekableByteBufferInputStream(segment.getOffHeapBuffer());
+                }
+            } else {
+                tmpFile = new File(ResourceUtil.getProcessPath(colmeta.getPath()));
+                input = new SeekableFileInput(tmpFile);
+            }
+        }
+        Assert.notNull(input, "Seekable input is null");
+        GenericDatumReader<GenericRecord> dreader = new GenericDatumReader<>(schema);
+        fileReader = new DataFileReader<>(input, dreader);
+        schema = fileReader.getSchema();
+    }
 
-	private void doInit(String resourcePath) throws Exception{
-		if(colmeta.getSourceType().equals(ResourceConst.IngestType.TYPE_HDFS.getValue())){
-			HDFSUtil util=new HDFSUtil(colmeta);
-			instream=util.getHDFSDataByRawInputStream(ResourceUtil.getProcessPath(resourcePath));
-			input=new AvroFSInput(new FSDataInputStream(instream),util.getHDFSFileSize(ResourceUtil.getProcessPath(resourcePath)));
-		}else {
-			// no hdfs input source
-			if(!ResourceConst.IngestType.TYPE_LOCAL.getValue().equals(colmeta.getSourceType())) {
-				instream = accessUtil.getRawInputStream(colmeta, ResourceUtil.getProcessPath(resourcePath));
-				int size=instream.available();
-				Double freeMemory= SysUtils.getFreeMemory();
-				//file size too large ,can not store in ByteBuffer or freeMemory too low
-				if (size >= Integer.MAX_VALUE || freeMemory<allowOffHeapDumpLimit) {
-					String tmpPath = FileUtils.getTempDirectoryPath() + ResourceUtil.getProcessFileName(resourcePath);
-					tmpFile = new File(tmpPath);
-					copyToLocal(tmpFile, instream);
-					input = new SeekableFileInput(tmpFile);
-				}else{
-					segment= MemorySegmentFactory.allocateOffHeapUnsafeMemory(size,this,new Thread(){});
-					ByteBuffer byteBuffer=segment.getOffHeapBuffer();
-					try(ReadableByteChannel channel= Channels.newChannel(instream)) {
-						IOUtils.readFully(channel, byteBuffer);
-						byteBuffer.position(0);
-					}
-					input=new SeekableByteBufferInputStream(segment.getOffHeapBuffer());
-				}
-			}else{
-				tmpFile=new File(ResourceUtil.getProcessPath(colmeta.getPath()));
-				input = new SeekableFileInput(tmpFile);
-			}
-		}
-		Assert.notNull(input,"Seekable input is null");
-		GenericDatumReader<GenericRecord> dreader=new GenericDatumReader<>(schema);
-		fileReader=new DataFileReader<>(input,dreader);
-		schema=fileReader.getSchema();
-	}
+    @Override
+    public Map<String, Object> next() {
+        Map<String, Object> retmap = new HashMap<>();
+        try {
+            GenericRecord records = fileReader.next();
+            List<Field> flist = schema.getFields();
+            for (Field f : flist) {
+                if (!ObjectUtils.isEmpty(records.get(f.name()))) {
+                    retmap.put(f.name(), records.get(f.name()).toString());
+                }
+            }
+        } catch (Exception ex) {
+            logger.error("", ex);
+        }
+        return retmap;
+    }
 
-	@Override
-	public Map<String, Object> next() {
-		Map<String,Object> retmap=new HashMap<>();
-		try{
-			GenericRecord records=fileReader.next();
-			List<Field> flist=schema.getFields();
-			for (Field f:flist) {
-				if(!ObjectUtils.isEmpty(records.get(f.name()))) {
-					retmap.put(f.name(), records.get(f.name()).toString());
-				}
-			}
-		}catch(Exception ex){
-			logger.error("",ex);
-		}
-		return retmap;
-	}
+    public Schema getSchema() {
+        return schema;
+    }
 
-	public Schema getSchema() {
-		return schema;
-	}
+    @Override
+    public void remove() {
+        try {
+            fileReader.next();
+        } catch (Exception ex) {
+            logger.error("", ex);
+        }
+    }
 
-	@Override
-	public void remove() {
-		try{
-			fileReader.next();
-		}catch(Exception ex){
-			logger.error("",ex);
-		}
-	}
+    @Override
+    public void close() throws IOException {
+        if (fileReader != null) {
+            fileReader.close();
+        }
+        if (!ObjectUtils.isEmpty(input)) {
+            input.close();
+        }
+        if (!ObjectUtils.isEmpty(tmpFile)) {
+            FileUtils.deleteQuietly(tmpFile);
+        }
+        super.close();
+    }
 
-	@Override
-	public void close() throws IOException {
-		if(fileReader!=null){
-			fileReader.close();
-		}
-		if(!ObjectUtils.isEmpty(input)){
-			input.close();
-		}
-		if(!ObjectUtils.isEmpty(tmpFile)){
-			FileUtils.deleteQuietly(tmpFile);
-		}
-		super.close();
-	}
-	 static class SeekableByteBufferInputStream extends InputStream implements SeekableInput {
-		private final byte[] oneByte = new byte[1];
+    static class SeekableByteBufferInputStream extends InputStream implements SeekableInput {
+        private final byte[] oneByte = new byte[1];
 
-		private ByteBuffer byteBuffer;
+        private ByteBuffer byteBuffer;
 
-		 SeekableByteBufferInputStream(ByteBuffer byteBuffer) throws IOException {
-			this.byteBuffer = byteBuffer;
-		}
+        SeekableByteBufferInputStream(ByteBuffer byteBuffer) throws IOException {
+            this.byteBuffer = byteBuffer;
+        }
 
-		public void seek(long p) throws IOException {
-			if (p < 0L) {
-				throw new IOException("Illegal seek: " + p);
-			} else {
-				byteBuffer.position((int) p);
-			}
-		}
+        public void seek(long p) throws IOException {
+            if (p < 0L) {
+                throw new IOException("Illegal seek: " + p);
+            } else {
+                byteBuffer.position((int) p);
+            }
+        }
 
-		public long tell() throws IOException {
-			return byteBuffer.position();
-		}
+        public long tell() throws IOException {
+            return byteBuffer.position();
+        }
 
-		public long length() throws IOException {
-			return byteBuffer.capacity();
-		}
+        public long length() throws IOException {
+            return byteBuffer.capacity();
+        }
 
-		public int read(byte[] b) throws IOException {
-			return read(b, 0, b.length);
-		}
+        public int read(byte[] b) throws IOException {
+            return read(b, 0, b.length);
+        }
 
-		public int read(byte[] b, int off, int len) throws IOException {
-			if (byteBuffer.remaining() == 0) {
-				return -1;
-			}
-			if (len > byteBuffer.remaining()) {
-				len = byteBuffer.remaining();
-			}
-			byteBuffer.get(b, off, len);
-			return len;
-		}
+        public int read(byte[] b, int off, int len) throws IOException {
+            if (byteBuffer.remaining() == 0) {
+                return -1;
+            }
+            if (len > byteBuffer.remaining()) {
+                len = byteBuffer.remaining();
+            }
+            byteBuffer.get(b, off, len);
+            return len;
+        }
 
-		public int read() throws IOException {
-			int n = this.read(this.oneByte, 0, 1);
-			return n == 1 ? this.oneByte[0] & 255 : n;
-		}
+        public int read() throws IOException {
+            int n = this.read(this.oneByte, 0, 1);
+            return n == 1 ? this.oneByte[0] & 255 : n;
+        }
 
-		public long skip(long skip) throws IOException {
-			long newPos = byteBuffer.position() + skip;
-			if (newPos > byteBuffer.remaining()) {
-				skip = byteBuffer.remaining();
-			}
-			byteBuffer.position(byteBuffer.position() + (int) skip);
-			return skip;
-		}
+        public long skip(long skip) throws IOException {
+            long newPos = byteBuffer.position() + skip;
+            if (newPos > byteBuffer.remaining()) {
+                skip = byteBuffer.remaining();
+            }
+            byteBuffer.position(byteBuffer.position() + (int) skip);
+            return skip;
+        }
 
-		public void close() throws IOException {
-			super.close();
-		}
+        public void close() throws IOException {
+            super.close();
+        }
 
-		public int available() throws IOException {
-			long remaining = length() - tell();
-			return remaining > 2147483647L ? Integer.MAX_VALUE : (int)remaining;
-		}
-	}
+        public int available() throws IOException {
+            long remaining = length() - tell();
+            return remaining > 2147483647L ? Integer.MAX_VALUE : (int) remaining;
+        }
+    }
 }
